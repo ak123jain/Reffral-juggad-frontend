fd4ea3ee945d0617b9468447b03d65098dba02a7
AkASH jain <aj9882854@gmail.com>
2025-04-20 20:20:54 +0530

    vite confid

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import MentorProfile from './Pages/mentorProfile'
 import SignUp from './Pages/SignUp'
@@ -11,9 +11,6 @@ import MentorAdd from './Pages/MentorAdd'
 
  
  
-
-
-
 const App = () => {
   return (
     <BrowserRouter>
@@ -21,7 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mentorprofile/:mentorId" element={<MentorProfile />} />
-           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/messege/:id" element={<Messege />} />
           <Route path="/addmentor" element={< MentorAdd />} />
