14d53c4b9a269e9cb68d5d692e6472bcdadcf5d3
AkASH jain <aj9882854@gmail.com>
2025-04-20 19:26:09 +0530

    correction in app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from './Pages/Login'
 import Messege from './Pages/messege'
 import Displaymentor from './Pages/Displaymentor'
 import Prenium from './Pages/Prenium'
-import MentorAdd from './Pages/Mentoradd'
+import MentorAdd from './Pages/MentorAdd'
 
  
  
@@ -24,7 +24,7 @@ const App = () => {
            <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/messege/:id" element={<Messege />} />
-          <Route path="/addmentor" element={<MentorAdd />} />
+          <Route path="/addmentor" element={< MentorAdd />} />
           <Route path="/displaymentor" element={<Displaymentor />} />
           <Route path="/premium/:MentorId" element={<Prenium />} />
         </Routes>
